6c7113f92ff46b8ea47862afcde816c5244f5c34
tejapathuri <tejatheyellowarmy@gmail.com>
2022-11-16 19:50:09 -0600

    updated changed

--- controllers/Dining.js
+++ controllers/Dining.js
@@ -22,9 +22,9 @@ exports.Dining_create_post = async function (req, res) {
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
     // {"Dining_type":"goat", "cost":12, "size":"large"}
-    document.name = req.body.name;
-    document.duration = req.body.duration;
-    document.director = req.body.director;
+    document.Dining_style = req.body.Dining_style;
+    document.Dining_type = req.body.Dining_type;
+    document.Dining_size = req.body.Dining_size;
     try {
         let result = await document.save();
         res.send(result);
@@ -33,15 +33,75 @@ exports.Dining_create_post = async function (req, res) {
         res.status(500);
         res.send(`{"error": ${err}}`);
     }
+    
 };
+
+// for a specific Dining. 
+exports.Dining_detail  = async function (req, res) {
+    console.log("dining"  + req.params.id) 
+    try { 
+        result = await Dining.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+
+// Handle Dining update form on PUT. 
+exports.Dining_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Dining.findById( req.params.id); 
+        console.log("teja");
+        // Do updates of properties 
+        if(req.body.Dining_style)  
+               toUpdate.Dining_style = req.body.Dining_style; 
+        if(req.body.Dining_type) toUpdate.Dining_type = req.body.Dining_type;
+        if(req.body.Dining_size) toUpdate.Dining_size = req.body.Dining_size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
+
+//Handle building the view for updating a Dining. 
+// query provides the id 
+exports.Dining_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Dining.findById(req.query.id) 
+        res.render('Diningupdate', { title: 'Dining Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
 // Handle Dining delete form on DELETE.
-exports.Dining_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Dining delete DELETE ' + req.params.id);
-};
-// Handle Dining update form on PUT.
-exports.Dining_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Dining update PUT' + req.params.id);
-};
+//exports.Dining_delete = function (req, res) {
+    //res.send('NOT IMPLEMENTED: Dining delete DELETE ' + req.params.id);
+//};
+
+// Handle Dining delete on DELETE. 
+exports.Dining_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Dining.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+//views
 exports.flim_view_all_Page = async function (req, res) {
     try {
         theDinings = await Dining.find();
@@ -52,3 +112,63 @@ exports.flim_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+
+ 
+ // Handle a show one view with id specified by query 
+ exports.Dining_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Dining.findById( req.query.id) 
+        res.render('Diningdetail',  
+{ title: 'Dining Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+//Handle building the view for creating a Dining. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.Dining_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('Diningcreate', { title: 'Dining Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a dining. 
+// query provides the id put
+exports.Dining_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Dining.findById(req.query.id) 
+        res.render('diningupdate', { title: 'Dining Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.Dining_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Dining.findById(req.query.id) 
+        res.render('Diningdelete', { title: 'Dining Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+ 
+
+ 
--- routes/Dining.js
+++ routes/Dining.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const Dining_controlers= require('../controllers/Dining');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+/* GET detail Dining page */ 
+router.get('/detail', Dining_controlers.Dining_view_one_Page); 
+
+/* GET create Dining page */ 
+router.get('/create', Dining_controlers.Dining_create_Page); 
+ 
+ 
+// GET create Dining page */ 
+router.get('/update', Dining_controlers.Dining_update_Page); 
+
+
+/* GET delete costume page */ 
+router.get('/delete', Dining_controlers.Dining_delete_Page); 
+ 
